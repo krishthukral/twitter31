e6a451411a9e7b7df59ad2bb1c65973043c466b6
krishthukral <59071300+krishthukral@users.noreply.github.com>
2023-12-11 15:57:39 -0800

    clerk

--- x/src/pages/_app.tsx
+++ x/src/pages/_app.tsx
@@ -5,7 +5,12 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+
+  return(
+<ClerkProvider{...pageProps}>
+<Component {...pageProps} />
+</ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
